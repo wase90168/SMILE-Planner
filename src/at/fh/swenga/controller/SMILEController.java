08eb7d25540a8fbddfbba306c950bbbaf3e3b100
Markus Skergeth <markus.skergeth@edu.fh-joanneum.at>
2017-06-14 20:33:15 +0200

    Implemented Logout Functionality
    The Logout Button in the Top right and bottom left menu now securely
    work

--- src/at/fh/swenga/controller/SMILEController.java
+++ src/at/fh/swenga/controller/SMILEController.java
@@ -2,11 +2,16 @@ package at.fh.swenga.controller;
 
 import java.util.Calendar;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -163,5 +168,15 @@ public class SMILEController {
 	public String handleLogin() {
 		return "login";
 	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){    
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login";
+	}
+
 
 }
